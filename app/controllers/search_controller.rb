fbafdccbbb9a28a76913f7ae3368951f1b780985
Jordan Quint <jordanquint@hotmail.com>
2012-04-16 21:22:56 -0500

    Added search page and form.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,15 @@
 class SearchController < ApplicationController
   def search
+    #params[:query] is a string that contains the text the user submitted
+    #params[:search_for] is what they search for. It can be either "title" or "content"
+    
+    
+    if params[:query].size > 0
+      @results = "Search results for '" + params[:query] + "' in the document " + params[:search_for] + "s."
+    end
   end
 
   def view
+    
   end
 end
