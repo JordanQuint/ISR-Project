2d9b6f10746ee2f8994d6362c981acd01201644a
Jordan Quint <jordanquint@hotmail.com>
2012-04-16 19:26:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ISRproject::Application.config.secret_token = '0f54ac6a282ce05cae23928fa7a276430e58b5303e6032a2a0a3b2d7a4da616efeb9b2498911993f004e15bbd8eebb8e56d82ba90dd607043fb54bd827864cc2'
