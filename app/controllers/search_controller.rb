c13fae54d0cc8409a89880f7f1b07bb132dffea7
Jordan Quint <jordanquint@hotmail.com>
2012-04-26 09:30:59 -0500

    Only show results when there was a search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,31 @@ class SearchController < ApplicationController
     #params[:query] is a string that contains the text the user submitted
     #params[:search_for] is what they search for. It can be either "title" or "content"
     
-    
-    if params[:query].size > 0
-      @results = "Search results for '" + params[:query] + "' in the document " + params[:search_for] + "s."
+    if !params[:query].nil? && params[:query].size > 0
+      terms = params[:query].split
+      term_string = ""
+
+      terms.each { |term|
+        term_string += term
+      }
+      
+      query_file = File.dirname(__FILE__) + "/../../../../Medline2012/example_query.txt"
+      File.open(query_file, 'w+') { |f| f.write(params[:query]) }
+      
+      path = File.dirname(__FILE__) + "/../../../../Medline2012/fasterRetrieval.mps < " + query_file
+      
+      response = `cd /home/mumps/Medline2012 ; mumps /home/mumps/Medline2012/fasterRetrieval.mps < ~/Medline2012/example_query.txt`
+
+      response = response.split("\n\n")
+      
+      @time_taken = response.last
+      
+      results = response[2..response.size - 2]
+      @results = []
+      results.each {|r|
+        @results << {:relevancy => r[0..4], :text => r[5..r.size - 1].split("\n")}
+      }
+      
     end
   end
 
